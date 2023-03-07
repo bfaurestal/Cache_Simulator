7317e461262941a80389a1b8baeea4d2fed30b0b
CompileMyLife <ibcinematografy@gmail.com>
2023-03-07 10:53:30 -0800

    fixed comments for repsective caches

--- parameters.sv
+++ parameters.sv
@@ -5,18 +5,18 @@ package mypkg;
 `define LINE_SIZE 64
 `define MESI_BITS 2
 
-parameter WAY   = `WAYS; // I_Cache
-parameter I_WAY   = `WAYS/2; //D_Cache set assiocivity
+parameter WAY   = `WAYS; // D_Cache
+parameter I_WAY   = `WAYS/2; //I_Cache set assiocivity
 parameter LINE = `LINE;
 parameter LINE_SIZE = `LINE_SIZE;
 parameter CAPACITY = LINE * WAY * LINE_SIZE;
 parameter MESI_BITS = `MESI_BITS;
 parameter ADDRESS_BITS = 32;
 parameter OFFSET_BITS  = $clog2(LINE_SIZE); //offset bits
-parameter INDEX_BITS = $clog2(LINE/WAY); //index bits 
-parameter I_INDEX_BITS = $clog2(LINE/I_WAY); // D_Cache index bits 
-parameter TAG_BITS = ADDRESS_BITS - (INDEX_BITS + OFFSET_BITS);
-parameter I_TAG_BITS = ADDRESS_BITS - (I_INDEX_BITS + OFFSET_BITS); //D_Cache tag
+parameter INDEX_BITS = $clog2(LINE/WAY); //D_cache index bits 
+parameter I_INDEX_BITS = $clog2(LINE/I_WAY); // I_Cache index bits 
+parameter TAG_BITS = ADDRESS_BITS - (INDEX_BITS + OFFSET_BITS); // D_Cache tag
+parameter I_TAG_BITS = ADDRESS_BITS - (I_INDEX_BITS + OFFSET_BITS); //I_Cache tag
 //using bits
 /* parameter integer i_size = 32; //instruction size
 parameter integer c_size = 24; // capacity size
@@ -34,14 +34,14 @@ reg [INDEX_BITS - 1 : 0]index;
 reg [TAG_BITS - 1: 0]tag;
 integer debug = 0;
 
-//Instruction cache structure 
+//Data cache structure 
 typedef struct {
  bit [TAG_BITS : 0] tag[LINE][WAY];
  reg [2 - 1: 0]protocol_bits[WAY];
  reg [WAY-2 : 0]PLRU; //subtract 2 because - 1 for array to 0 and -1 for the equation for PLRU bits
 } D_Cache;
 
-//Data cache structure
+//Instruction cache structure
 typedef struct {
  //reg [I_TAG_BITS : 0] tag[I_WAY];
  logic [14:0] iCache[16][2];
