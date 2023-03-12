5e305de6dd86bafb3d332525d337a9479cff2aaf
ramezabdo <91917000+ramezabdo@users.noreply.github.com>
2023-03-11 20:07:22 -0800

    Update mesi_protocol_i.sv

--- mesi_protocol_i.sv
+++ mesi_protocol_i.sv
@@ -1,7 +1,8 @@
-module mesi_protocol(clk, reset, inbits, detect);
+module mesi_protocol(clk, reset, inbits, r_w, detect);
 
 input clk;
 input reset;
+input [1:0] r_w;
 input [2:0] inbits;
 output reg detect;
 
@@ -15,48 +16,73 @@ end
 always @(posedge clk, posedge reset)
 begin
 	if (reset)
-		state <= 2'b10;
+		state <= 2'b00;
 	else
 	begin
-		case (state)
-			2'b00:
-			begin
-				if (inbits == 3'b001)
-					state <= 2'b01;
-				else if (inbits == 3'b010)
-					state <= 2'b10;
-				else if (inbits == 3'b101)
-					state <= 2'b11;
-				else
-					state <= 2'b00;
-			end
-			2'b01:
-			begin
-				if (inbits == 3'b010)
-					state <= 2'b01;
-				else if (inbits == 3'b011)
-					state <= 2'b11;
-				else
-					state <= 2'b01;
-			end
-			2'b10:
-			begin
-				if (inbits == 3'b101)
-					state <= 2'b10;
-				else if (inbits == 3'b110)
-					state <= 2'b11;
-				else
-					state <= 2'b10;
-			end
-			2'b11:
-			begin
-				if (inbits == 3'b111)
-					state <= 2'b11;
-				else
-					state <= 2'b11;
-			end
-		endcase
-	end
+		if (r_w == 1'b0)
+			case (state)
+				2'b00:
+				begin
+					if (inbits == 3'b000) 
+						state <= 2'b01;
+					else if (inbits == 3'b010)
+						state <= 2'b10;
+					else
+						state <= 2'b00;
+				end
+				2'b01:
+				begin
+					if (inbits == 3'b010)
+						state <= 2'b01;
+					else
+						state <= 2'b00;
+				end
+				2'b10:
+				begin
+					if (inbits == 3'b101)
+						state <= 2'b10;
+					else if (inbits == 3'b110)
+						state <= 2'b01;
+					else
+						state <= 2'b00;
+				end
+				2'b11:
+				begin
+					if (inbits == 3'b111)
+						state <= 2'b11;
+					else
+						state <= 2'b00;
+				end
+			endcase
+		else
+			case (state)
+				2'b00:
+				begin
+					if (inbits == 3'b000) 
+						state <= 2'b10;
+					else
+						state <= 2'b00;
+				end
+				2'b01:
+				begin
+					if (inbits == 3'b010)
+						state <= 2'b10;
+					else
+						state <= 2'b00;
+				end
+				2'b10:
+				begin
+					if (inbits == 3'b101)
+						state <= 2'b11;
+					else
+						state <= 2'b00;
+				end
+				2'b11:
+				begin
+					state <= 2'b00;	
+				end
+			endcase
+		end
 end
 
 always @(posedge clk, posedge reset)
