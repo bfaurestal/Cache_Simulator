6c4b82d41b5ff0a53de0e76bd6cf375b6552d02c
CompileMyLife <ibcinematografy@gmail.com>
2023-03-13 00:10:59 -0700

    used a module to express the update LRU algorithm. Might need changes later on from index to set inputs

--- update_LRU1.sv
+++ update_LRU1.sv
@@ -1,32 +1,40 @@
-module update_LRU(block_sel_4, block_sel_8, LRU_b_4, LRU_b_8, up_LRU_b_4, up_LRU_b_8);
+module update_LRU(index_4, way_4, index_8, way_8, LRU_b_4, LRU_b_8);
 
-output reg [$clog2(4) - 1: 0] up_LRU_b_4;
-output reg [$clog2(8) - 1: 0] up_LRU_b_8;
+input [3:0] way_4;
+input [7:0] way_8;
 
-input [$clog2(4) - 1: 0] LRU_b_4;
-input [$clog2(8) - 1: 0] LRU_b_8;
+// [4 ways][2 bits]
+// [8 ways][3 bits]
+input int LRU_b_4 [3:0][$clog2(4) - 1: 0];
+input int LRU_b_8 [7:0][$clog2(8) - 1: 0];
 
-input [4 - 1: 0] block_sel_4;
-input [8 - 1: 0] block_sel_8;
+input [13: 0] index_4;
+input [13: 0] index_8;
 
-reg [$clog2(4) - 1: 0] LRU_b_4_temp;
-reg [$clog2(8) - 1: 0] LRU_b_8_temp;
+int i;
 
-reg [$clog2(4) - 1: 0] LRU_b_4_final;
-reg [$clog2(8) - 1: 0] LRU_b_8_final;
-
-int i = 0;
-int j = 0;
-
-always @(block_sel_4, LRU_b_4)
+always @(index_4, way_4)
 begin
-    for (i = 0; i < block_sel_4; i = i+1)
+    for (i = 0; i < 4; i = i+1)
     begin 
-        for (j = 0; j < $clog2(4) - 1; j = j+1)
-        begin
-            // Check if empty
-            if (block_sel_4...)
+        if (LRU_b_4[index_4][i] < LRU_b_4[index_4][way_4])
+		LRU_b_4[index_4][i] = LRU_b_4[index_4][i] + 1;
+	
+	LRU_b_4[index_4][way_4] = 0;
+    end
+end
 
-            // Access 
 
+always @(index_8, way_8)
+begin
+    for (i = 0; i < 8; i = i+1)
+    begin 
+        if (LRU_b_8[index_8][i] < LRU_b_8[index_8][way_8])
+		LRU_b_8[index_8][i] = LRU_b_8[index_8][i] + 1;
+	
+	LRU_b_8[index_8][way_8] = 0;
+    end
+end
+
+endmodule
 
